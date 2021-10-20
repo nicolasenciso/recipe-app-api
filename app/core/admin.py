92e5b4393ded8ff37fad76eec5398e99e8a796e9
Nic <nicolas.ricardo@tus-cuentas.com>
2021-10-20 12:11:01 -0500

    adding super user tests and model and admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,1 +1,23 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
+from django.utils.translation import gettext as _
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Personal Info'), {'fields':('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': ('is_active', 'is_staff', 'is_superuser')
+            }
+        ),
+        (('Import dates'), {'fields': ('last_login',)})
+    )
+
+
+admin.site.register(models.User, UserAdmin)
